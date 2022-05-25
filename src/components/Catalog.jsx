3c23ab388caa0581c25c5b81c3b97c4030b1b1fd
Настя Давыдова <logzka@mail.ru>
2022-05-25 10:56:53 +0300

    Update package-lock.json

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 
-import { Transition } from 'react-transition-group';
 import '../styles/catalog.css';
 
 import Card from "./Card";
@@ -10,55 +9,30 @@ const Catalog = () => {
 
     setTimeout(() => {
         setCardsData(
-                [
-                    {
-                        value: 'e-commerce',
-                    }, {
-                        value: 'design',
-                    }, {
-                        value: 'website',
-                    }, {
-                        value: 'admin',
-                    }, {
-                        value: 'manage',
-                    },
-                ]
+            [
+                {
+                    value: 'e-commerce',
+                }, {
+                    value: 'design',
+                }, {
+                    value: 'website',
+                }, {
+                    value: 'admin',
+                }, {
+                    value: 'manage',
+                },
+            ]
         );
     }, 2000);
-
-    const defaultStyle = {
-        transition: 'opacity 3s ease-in-out',
-        opacity: 0,
-    }
-
-    const transitionStyles = {
-        entering: { opacity: 1 },
-        entered: { opacity: 1 },
-        exiting: { opacity: 0 },
-        exited: { opacity: 0 },
-    };
 
     return (
         <div className="catalog catalog--wrapper w-100 grid gap">
             {
                 cardsData.map(card =>
-                    <Transition
-                        in={false}
-                        timeout={3000}
+                    <Card
                         key={card.value}
-                    >
-                        {state => (
-                            <Card
-                                key={card.value}
-                                card={card}
-
-                                style={{
-                                    ...defaultStyle,
-                                    ...transitionStyles[state]
-                                }}
-                            />
-                        )}
-                    </Transition>
+                        card={card}
+                    />
                 )
             }
         </div>
