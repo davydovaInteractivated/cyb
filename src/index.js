2d1b15d37ac607bd505ada1e4b644ad4927f9bcb
Anastasiia Davydova <logzkada@gmail.com>
2023-08-23 22:22:38 +0300

    Set .env

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
       </BrowserRouter>
     </I18nextProvider>
