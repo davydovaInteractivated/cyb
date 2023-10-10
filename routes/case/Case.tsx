aa644e6d2e150fb1cd3ac4d413493a4873ba9a2f
davydovaInteractivated <113861326+davydovaInteractivated@users.noreply.github.com>
2023-10-10 09:52:51 +0000

    Deploying to gh-pages from  @ d6f53ec946cc887cfa4ac4f5265808950a531739 🚀

--- routes/case/Case.tsx
+++ routes/case/Case.tsx
@@ -2,6 +2,9 @@ import { withTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 
+/** Utils */
+import { tableDataToPdf } from '../../utils/download';
+
 /** Styles */
 import '../../styles/case.scss';
 
@@ -12,12 +15,14 @@ import {
 
 /** Contexts */
 import { CasesContext } from '../../context/cases.context';
+import { SettingsContext } from '../../context/settings.context';
 
 /** Components */
 import CustomButton from '../../components/custom/custom-button/CustomButton';
 
 /** Types */
-import { TCase, TCaseDataKey } from '../../context/cases.context';
+import { TCase, TCaseDataKey, TCaseDataField } from '../../context/cases.context';
+import { TDataItem } from '../../ts/types/download';
 
 interface ICaseProps {
     caseItem: TCase,
@@ -28,6 +33,9 @@ interface ICaseProps {
 const Case = ({ caseItem, caseIndex, t }: ICaseProps) => {
     const { title, data } = caseItem || {};
     const { cases, setCases, getTotalCost } = useContext(CasesContext);
+    const { activeTheme, themes } = useContext(SettingsContext);
+    const colorsFromCurrentTheme = themes.find((th) => th.name === activeTheme)?.colors;
+    const { light } = colorsFromCurrentTheme || {};
 
     /**
      * Delete Case
@@ -38,6 +46,66 @@ const Case = ({ caseItem, caseIndex, t }: ICaseProps) => {
             .filter((_, index) => caseIndex !== index);
 
         setCases(filteredCases);
+    };
+
+    const getDownloadData = () => {
+        const dataKeys = Object.keys(caseItem.data);
+        const dataValues = Object.values(caseItem.data);
+
+        const data: TDataItem[][] = dataValues
+            .map((value: TCaseDataField, index: number) => ([
+                {
+                    content: dataKeys[index],
+                    cellStyle: { paddingLeft: 10 },
+                }, {
+                    content: value.value,
+                    cellStyle: { fill: 'white' },
+                }, {
+                    content: value.cost,
+                    cellStyle: { fill: 'white' },
+                }
+            ]));
+
+        data.push([{
+            content: 'total pre-calculate ˜cost in $',
+            cellStyle: {
+                paddingLeft: 10,
+                color: light,
+                weight: 'bold',
+            },
+        }, {
+            content: '-', cellStyle: {},
+        }, {
+            content: `${getTotalCost(caseItem)}`,
+            cellStyle: { weight: 'bold', color: light },
+        }]);
+
+        return {
+            headers: [[{
+                content: 'case params',
+                colSpan: 1,
+            }, {
+                content: 'case values',
+                colSpan: 1,
+            }, {
+                content: 'pre-calculate cost in $',
+                colSpan: 1,
+            }]],
+            data,
+        }
+    };
+
+    /**
+     * Download Case
+     */
+    const downloadCase = async () => {
+        const { data, headers } = getDownloadData();
+
+        tableDataToPdf(
+            `${caseItem.title}-case.pdf`,
+            'p',
+            { data, headers },
+        );
     };
 
     return (
@@ -63,7 +131,16 @@ const Case = ({ caseItem, caseIndex, t }: ICaseProps) => {
                         <span>&nbsp;(~&nbsp;${data[key as TCaseDataKey].cost})</span>
                     </div>)
                 }
-                <strong className='case--total'>{t('total')}:&nbsp;&nbsp;~&nbsp;${getTotalCost(caseItem)}</strong>
+                <div className='case--content__footer w-100 grid gap'>
+                    <strong className='case--content__total'>{t('total')}:&nbsp;&nbsp;~&nbsp;${getTotalCost(caseItem)}</strong>
+                    <CustomButton
+                        className='case--content__download'
+                        size='small'
+                        onClick={() => downloadCase()}
+                    >
+                        download PDF
+                    </CustomButton>
+                </div>
             </div>
         </div>
     );
---
+++ utils/download.ts
@@ -0,0 +1,138 @@
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
+// import * as XLSX from 'xlsx-js-style';
+
+/** Types */
+import { TDataItem } from '../ts/types/download';
+
+/**
+ * Create Table in pdf file by table data
+ * @param fileName
+ * @param orientation
+ * @param table
+ * @returns {void}
+ */
+export const tableDataToPdf = (
+  fileName: string,
+  orientation: 'p' | 'l',
+  table: { data: TDataItem[][], headers: {
+  content: string,
+  colSpan: number,
+  }[][] },
+): void => {
+  const doc = new jsPDF(
+    orientation,
+    'px',
+    'a4',
+  );
+
+  autoTable(doc, {
+    head: [...table.headers],
+    body: [
+      ...table.data,
+    ],
+    theme: 'grid',
+    // tableWidth: 'wrap',
+    horizontalPageBreak: true,
+    horizontalPageBreakRepeat: 0,
+    margin: 20,
+    useCss: true,
+    // pageBreak: 'avoid',
+    // rowPageBreak: 'avoid',
+    headStyles: {
+      halign: 'center',
+      fillColor: '#ffffff',
+      textColor: '#000000',
+      lineColor: '#e4e4e4',
+      lineWidth: 0.2,
+      minCellWidth: 50,
+    },
+    bodyStyles: {
+      lineColor: '#e4e4e4',
+      lineWidth: 0.2,
+      minCellWidth: 50,
+    },
+
+    didParseCell: (HookData) => {
+      const { section } = HookData;
+      const { cell } = HookData;
+      const { column } = HookData;
+
+      if (section === 'head') {
+        // cell.minWidth = 50;
+        // cell.contentWidth = 50;
+        return;
+      }
+
+      const { styles: pdfCellStyles } = cell;
+
+      const cellRawAsString = JSON.parse(JSON.stringify(cell.raw || null));
+
+      if (!cellRawAsString) throw Error('No cell data from table.');
+
+      const { cellStyle } = cellRawAsString || {};
+
+      if (column.index === 0) pdfCellStyles.cellPadding = { left: cellStyle.paddingLeft, top: 5 };
+
+      pdfCellStyles.textColor = cellStyle.color;
+      pdfCellStyles.fontStyle = cellStyle.weight;
+      pdfCellStyles.fillColor = cellStyle.fill;
+  },
+  
+    didDrawPage: (HookData) => {
+      /** Set Page Number */
+      doc.setFont('helvetica');
+      doc.setFontSize(10);
+      doc.setTextColor('#BDBDBD');
+      doc.text(`${HookData.table.pageCount}`, 30, 15, {
+        align: 'right',
+        lineHeightFactor: 0.8,
+      });
+    },
+  });
+  
+  const dateTime = new Date().toDateString();
+
+  doc.save(`${fileName}_${dateTime}.pdf`);
+};
+  
+// /**
+//  * Create Table in Xls file by table data
+//  * @param fileName
+//  * @param table
+//  * @returns {void}
+//  */
+// export const tableDataToXls = (
+//     fileName: string,
+//     table: { v: string | number, t: string, s?: {
+//       fill?: object,
+//       font?: object,
+//       border?: object,
+//       alignment?: object,
+//     } }[][],
+//     merge?: {
+//       s: { r: number, c: number },
+//       e: { r: number, c: number },
+//     }[] | null,
+//   ) => {
+//     const worksheet = XLSX.utils.aoa_to_sheet(table, { cellStyles: true });
+//     const workbook = XLSX.utils.book_new();
+//     XLSX.utils.book_append_sheet(workbook, worksheet, '1', true);
+  
+//     const dateTime = getDateWithDayOfWeek();
+  
+//     const rowHeightList = Array(table.length + 1);
+//     const colsWidthsList = Array(table[0].length);
+//     rowHeightList.fill({ hpx: 20 });
+//     colsWidthsList.fill({ wpx: 70 });
+//     colsWidthsList.unshift({ wpx: 155 });
+//     worksheet['!rows'] = rowHeightList;
+//     worksheet['!cols'] = colsWidthsList;
+  
+//     if (merge) worksheet['!merges'] = merge;
+  
+//     XLSX.writeFile(workbook, `${fileName}_${dateTime}.xlsx`, {
+//       compression: true,
+//       cellStyles: true,
+//     });
+// };
