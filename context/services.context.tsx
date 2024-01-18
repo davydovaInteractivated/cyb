97ef3fb452ab2b7558bbfdc5f672f1aa78b4ae1d
davydovaInteractivated <113861326+davydovaInteractivated@users.noreply.github.com>
2024-01-18 14:32:15 +0000

    Deploying to gh-pages from  @ a20d458a5ae5975a318b5ad54c5f5dc55abee4b2 🚀

--- context/services.context.tsx
+++ context/services.context.tsx
@@ -136,7 +136,7 @@ export const ServicesContextProvider = ({ children }: PropsWithChildren) => {
         }
     
         setFilteredServices(filteredServices);
-    }, [onFavorites]);
+    }, [onFavorites, activeLang]);
 
     useEffect(() => {
         // addCollectionAndDocuments('services', servicesData);
